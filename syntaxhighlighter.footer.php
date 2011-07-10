edf441ef6b49eed7be4f3e77b3f6450052ee4d31
Vladimir Sibirov <trustmaster@kodigy.com>
2011-07-11 01:19:24 +0400

    Added the plugin to Git

---
+++ syntaxhighlighter.footer.php
@@ -0,0 +1,29 @@
+<?php
+/* ====================
+[BEGIN_COT_EXT]
+Hooks=footer.first
+[END_COT_EXT]
+==================== */
+
+/**
+ * SyntaxHighlighter connector for Cotonti
+ *
+ * @package syntaxhighligther
+ * @version 1.0
+ * @author Trustmaster
+ * @copyright Copyright (c) Cotonti Team 2011
+ * @license BSD
+ */
+
+defined('COT_CODE') or die('Wrong URL');
+
+$sh_core_css = cot_rc('code_rc_css_file', array('url' => $cfg['plugins_dir'] . '/syntaxhighlighter/lib/styles/shCore.css'));
+$sh_theme_css = cot_rc('code_rc_css_file', array('url' => $cfg['plugins_dir'] . '/syntaxhighlighter/lib/styles/shThemeDefault.css'));
+$sh_core_js = $cfg['plugins_dir'] . '/syntaxhighlighter/lib/scripts/shAll.min.js';
+
+cot_rc_embed_footer(<<<JS
+$(function(){if($('pre').length>0){ $('head').append('$sh_core_css');$('head').append('$sh_theme_css');var sh_e1=document.createElement('script');sh_e1.async=true;sh_e1.src ='$sh_core_js';document.getElementById('body').appendChild(sh_e1);}});
+JS
+);
+
+?>
---
+++ syntaxhighlighter.setup.php
@@ -0,0 +1,22 @@
+<?php
+/* ====================
+[BEGIN_COT_EXT]
+Code=syntaxhighlighter
+Name=SyntaxHighlighter
+Description=Code highlighter written in JavaScript
+Version=1.1-3.0.83
+Date=2011-07-08
+Author=Alex Gorbatchev
+Copyright=
+Notes=
+SQL=
+Auth_guests=R
+Lock_guests=W12345A
+Auth_members=RW
+Lock_members=12345A
+[END_COT_EXT]
+==================== */
+
+defined('COT_CODE') or die('Wrong URL');
+
+?>
