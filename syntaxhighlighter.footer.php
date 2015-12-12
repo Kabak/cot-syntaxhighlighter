a646e26e6d6cd6715939b0118d999bd5f789dc27
Andrew Matsovkin <macik.spb@gmail.com>
2015-12-12 22:21:05 +0300

    Adding Color themes usage
    Adding feature to select predefined color themes and allow to use user
    defined CSS.

--- syntaxhighlighter.footer.php
+++ syntaxhighlighter.footer.php
@@ -17,13 +17,32 @@ Hooks=footer.first
 
 defined('COT_CODE') or die('Wrong URL');
 
-$sh_core_css = cot_rc('code_rc_css_file', array('url' => $cfg['plugins_dir'] . '/syntaxhighlighter/lib/styles/shCore.css'));
-$sh_theme_css = cot_rc('code_rc_css_file', array('url' => $cfg['plugins_dir'] . '/syntaxhighlighter/lib/styles/shThemeDefault.css'));
+$shTheme = $cfg['plugin']['syntaxhighlighter']['theme'];
+$shTheme = $shTheme ? $shTheme : 'Default';
+
+/**
+ * Returns full path to theme css file
+ * Allowing override it with user custom css file located in `themes/themename/styles` folder
+ *
+ * @param string $chTheme Theme name (without prefix)
+ * @return string Full path to theme css file
+ */
+function shl_cssname($chTheme = 'Default')
+{
+	global $cfg, $theme;
+	$cssfile = $cfg['themes_dir'] . '/' . $theme . '/styles/shCore' . $chTheme . '.css';
+	if (is_file($cssfile)) return $cssfile;
+	$cssfile = $cfg['plugins_dir'] . '/syntaxhighlighter/lib/styles/shCore' . $chTheme . '.css';
+	return $cssfile;
+}
+
+$sh_core_css = cot_rc('code_rc_css_file', array(
+	'url' => shl_cssname($shTheme)
+));
 $sh_core_js = $cfg['plugins_dir'] . '/syntaxhighlighter/lib/scripts/shAll.min.js';
 
 cot_rc_embed_footer(<<<JS
-$(function(){if($('pre').length>0){ $('head').append('$sh_core_css');$('head').append('$sh_theme_css');var sh_e1=document.createElement('script');sh_e1.async=true;sh_e1.src ='$sh_core_js';$('body').append(sh_e1);}});
+$(function(){if($('pre').length>0){ $('head').append('$sh_core_css');var sh_e1=document.createElement('script');sh_e1.async=true;sh_e1.src ='$sh_core_js';document.getElementById('body').appendChild(sh_e1);}});
 JS
 );
 
-?>
